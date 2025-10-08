90a9d7610185af58e17534481cca5013efbf7e4d
Roksana Żyłka <roksana.zylka@studenci.collegiumwitelona.pl>
2025-10-08 09:19:16 +0200

    zainicjowany projekt
    dwa foldery backend i frontend
    mozecie dzialac

---
+++ backend/src/main/java/com/karaokeapp/karaoke_backend/KaraokeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.karaokeapp.karaoke_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/karaokeapp/karaoke_backend/KaraokeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karaokeapp.karaoke_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
