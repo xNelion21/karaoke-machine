8232bdecd1616980f37aec644fd6fadbb477ea02
Roksana Żyłka <roksana.zylka@studenci.collegiumwitelona.pl>
2026-01-17 16:35:15 +0100

    poprawki
    poprawki do wpisywania sugestii

--- backend/src/main/java/com/karaokeapp/karaoke_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/karaokeapp/karaoke_backend/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("PATCH", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
