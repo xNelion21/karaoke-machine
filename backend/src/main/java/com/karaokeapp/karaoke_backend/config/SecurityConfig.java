ab06cfd4e04ed8a50011994be4dc32dff0e7ce95
Roksana Żyłka <roksana.zylka@studenci.collegiumwitelona.pl>
2026-01-17 20:30:20 +0100

    Update SecurityConfig.java

--- backend/src/main/java/com/karaokeapp/karaoke_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/karaokeapp/karaoke_backend/config/SecurityConfig.java
@@ -39,8 +39,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
+                "https://karaoke-machine-frontend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("PATCH", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
