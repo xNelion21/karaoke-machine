6c570150ce09c85dca24101c4cef4296e2bb193b
Mikołaj Wołoszyn <mikolaj.woloszyn@studenci.collegiumwitelona.pl>
2026-01-17 18:10:03 +0100

    fix: zabezpieczenie przed błędem przy braku tekstu piosenki, poprawka czcionki dla navbara

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -112,6 +112,7 @@ onUnmounted(() => {
 
 <style scoped>
 .navbar {
+  font-family: 'Arial', sans-serif;
   background: rgba(24, 24, 24);
   backdrop-filter: blur(10px);
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
